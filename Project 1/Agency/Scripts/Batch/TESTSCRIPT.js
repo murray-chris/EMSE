4b6d0db82e6fd581137866c725f8cde644e223fe
Chris <cmurray@accela.com>
2019-02-05 15:33:25 -0700

    Added jsdoc comment

--- Project 1/Agency/Scripts/Batch/TESTSCRIPT.js
+++ Project 1/Agency/Scripts/Batch/TESTSCRIPT.js
@@ -1,5 +1,13 @@
 addTwoNumbers(1,5);
 
+
+/**
+ * addTwoNumbers - Adds two numbers together.
+ *
+ * @param  {type} a 1st number
+ * @param  {type} b 2nd number
+ * @return {type}   number 
+ */
 function addTwoNumbers(a, b) {
   return a + b
 }
